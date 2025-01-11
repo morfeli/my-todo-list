ae4853a14e1c13b6e56124fda32686276f00fabf
morfeli <fmoreira95@hotmail.com>
2025-01-11 18:59:20 -0500

    feat: wip

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -1,19 +1,6 @@
-import { clsx } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-export type ClassDictionary = Record<string, any>;
-export type ClassArray = ClassValue[];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export type ClassValue =
-  | ClassArray
-  | ClassDictionary
-  | string
-  | number
-  | bigint
-  | null
-  | boolean
-  | undefined;
